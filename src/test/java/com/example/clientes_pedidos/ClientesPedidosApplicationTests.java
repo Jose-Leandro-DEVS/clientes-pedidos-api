c3b11762b788740b4fab4f6ce1153bbd13477349
José <joseleandro.devs@gmail.com>
2025-06-08 17:00:56 -0300

    First commit

---
+++ src/test/java/com/example/clientes_pedidos/ClientesPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientes_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
